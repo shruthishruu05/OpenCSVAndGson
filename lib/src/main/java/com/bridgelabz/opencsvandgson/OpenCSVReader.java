d2ab3c95ba1050d10b41c0af0fa648539a184955
shruthishruu05 <shruthi.m@perfios.com>
2021-09-20 19:32:58 +0530

    ADDED : Project structure

---
+++ lib/src/main/java/com/bridgelabz/opencsvandgson/OpenCSVReader.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.opencsvandgson;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class OpenCSVReader
+{
+	private static final String SAMPLE_CSV_FILE = "users.csv";
+	public static void main(String[] args) throws IOException, CsvException {
+		try (
+			Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE));
+			CSVReader csvReader = new CSVReader(reader);
+		) {
+			List<String[]> records = csvReader.readAll();
+			for (String[] record: records) {
+				System.out.println("Name: "+record[0]);
+				System.out.println("Email: "+record[1]);
+				System.out.println("Phone: "+record[2]);
+				System.out.println("Country: "+record[3]);
+				System.out.println("===================");
+			}
+		}
+	}
+}
+	
